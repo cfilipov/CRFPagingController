3ccd4f66f997104200232307568d7ea4090b05d3
Cristian Filipov <filipov@gmail.com>
2013-04-03 19:21:57 -0700

    Initial Commit

---
+++ CRFPagingController/CRFPagingController.h
@@ -0,0 +1,13 @@
+//
+//  CRFPagingController.h
+//  CRFPagingController
+//
+//  Created by vopilif on 4/3/13.
+//  Copyright (c) 2013 Cristian Filipov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRFPagingController : NSObject
+
+@end
---
+++ CRFPagingController/CRFPagingController.m
@@ -0,0 +1,13 @@
+//
+//  CRFPagingController.m
+//  CRFPagingController
+//
+//  Created by vopilif on 4/3/13.
+//  Copyright (c) 2013 Cristian Filipov. All rights reserved.
+//
+
+#import "CRFPagingController.h"
+
+@implementation CRFPagingController
+
+@end
