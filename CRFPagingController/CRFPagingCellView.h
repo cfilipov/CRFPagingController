c0a2f97db86ea585fa70544f15576c00944efcb2
Cristian Filipov <filipov@gmail.com>
2013-04-05 03:37:10 -0700

    Added 3-clause BSD license.

--- CRFPagingController/CRFPagingCellView.h
+++ CRFPagingController/CRFPagingCellView.h
@@ -1,9 +1,36 @@
 //
-//  CRFPagingCell and CRFPagingCellView
 //  CRFPagingController
 //
-//  Created by vopilif on 4/4/13.
-//  Copyright (c) 2013 Cristian Filipov. All rights reserved.
+//  Copyright (c) 2013, Cristian Filipov. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the Cristian Filipov nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+//  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+//  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+//  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  ////////////////////////////////////////////////////////////////////////////
 //
 //  CRFPagingCellView is the default implementation of the CRFPagingCell
 //  protocol. The CRFPagingController uses a UITableViewCell instance which
@@ -16,7 +43,7 @@
 
 #import <UIKit/UIKit.h>
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 @protocol CRFPagingCell <NSObject>
 
@@ -24,7 +51,7 @@
 
 @end
 
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 @interface CRFPagingCellView : UITableViewCell <CRFPagingCell>
 
---
+++ README.md
@@ -0,0 +1,58 @@
+CRFPagingController
+===================
+
+Summary
+-------
+
+CRFPagingController provides "infinite scrolling" functionality to UITableViews 
+by implementing the UITableViewDataSource protocol and forwarding methods to a 
+"real" data source. This allows one to easily enable table view paging by simply 
+placing this controller between the data source and table view.
+
+Installation
+------------
+
+Copy the files under the CRFPagingController directory into your project, adding 
+them to your target.
+
+Usage
+-----
+
+Implement the CRFPagingControllerDelegate protocol (in addition to the 
+UITableView delegate and dataSource):
+
+    @interface MyDataSource : NSObject <CRFPagingControllerDelegate, UITableViewDelegate, UITableViewDataSource>
+
+    ...
+
+    @end
+
+Instantiate the CRFPagingController:
+
+    self.pagingController = [[CRFPagingController alloc] init];
+
+Set the delegate, tableViewDataSource and tableViewDelegate properties:
+
+    self.pagingController.delegate = self.dataSource;
+    self.pagingController.tableViewDataSource = self.dataSource;
+    self.pagingController.tableViewDelegate = self.dataSource;
+
+Set the dataSource and delegate of your UITableView to the CRFPagingController 
+instance.
+
+    self.mainViewController.tableView.dataSource = self.pagingController;
+    self.mainViewController.tableView.delegate = self.pagingController;
+
+Configure the CRFPagingController:
+
+    self.pagingController.buffer = 5;
+
+IMPORTANT: Because UITableViews cache results from `-respondsToSelector:` you MUST 
+set the tableViewDataSource and tableViewDelegate properties of this class 
+BEFORE you set the dataSource and delegate properties of the UITableView.
+
+License
+-------
+
+CRFPagingController is licensed under the 3-clause BSD license. A copy of the 
+license can be found in the LICENSE file.
